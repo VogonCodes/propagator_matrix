9a8bbf662b25a904890fa2fca52fcb273eaa6fb5
VogonCodes <bakocad@gmail.com>
2026-07-27 21:31:54 +0200

    change of plans + printing

--- src/body_mod.f90
+++ src/body_mod.f90
@@ -2,14 +2,9 @@ module body_mod
     use constants, only: dp
     implicit none
 
-    type :: layer_t
-        real(dp) :: radius, density, shear_modulus, gravity, viscosity, bulk_modulus
-    end type
-
     type :: body_t
         integer :: n_layers
-        type(layer_t), allocatable :: layers(:)
-        real(dp) :: bottom_radius, surface_radius
+        real(dp), allocatable :: radius(:), density(:), shear_modulus(:), gravity(:), viscosity(:), bulk_modulus(:)
     end type
 
     contains
@@ -21,23 +16,57 @@ module body_mod
                 error stop "Number of layers must be positive integer!"
             endif
 
-            if (allocated(body%layers)) then
+            ! stupid, but assume for now that is this is allocated, then everything else is too
+            if (allocated(body%radius)) then
                 error stop "Already allocated!"
             endif
 
             body%n_layers = N
-            allocate(body%layers(N))
+            allocate(body%radius(N))
+            allocate(body%density(N))
+            allocate(body%shear_modulus(N))
+            allocate(body%gravity(N))
+            allocate(body%viscosity(N))
+            allocate(body%bulk_modulus(N))
         end subroutine
 
         subroutine destroy_body(body)
             type(body_t), intent(inout) :: body
 
-            if (allocated(body%layers)) then
-                deallocate(body%layers)
+            if (allocated(body%radius)) then
+                deallocate(body%radius)
+            else if (allocated(body%density)) then
+                deallocate(body%density)
+            else if (allocated(body%shear_modulus)) then
+                deallocate(body%shear_modulus)
+            else if (allocated(body%gravity)) then
+                deallocate(body%gravity)
+            else if (allocated(body%viscosity)) then
+                deallocate(body%viscosity)
+            else if (allocated(body%bulk_modulus)) then
+                deallocate(body%bulk_modulus)
             end if
             body%n_layers = 0
-            body%bottom_radius = 0
-            body%surface_radius = 0
+        end subroutine
+
+        subroutine write_body_t(body, unit)
+            type(body_t), intent(in) :: body
+            integer, intent(in), optional :: unit
+            integer :: u
+
+            if (present(unit)) then
+                u = unit
+            else
+                u = 6 ! default stdo
+            endif
+
+            write(u,* ) "No of layers:", body%n_layers
+            write(u, '(A, 5F12.4)') "radii (first 5 values): ", body%radius(1:min(5,body%n_layers))
+            write(u, '(A, 5F12.4)') "densities (first 5 values): ", body%density(1:min(5,body%n_layers))
+            write(u, '(A, 5F12.4)') "shear moduli (first 5 values): ", body%shear_modulus(1:min(5,body%n_layers))
+            write(u, '(A, 5F12.6)') "gravity (first 5 values): ", body%gravity(1:min(5,body%n_layers))
+            write(u, '(A, 5F12.4)') "viscosities (first 5 values): ", body%viscosity(1:min(5,body%n_layers))
+            write(u, '(A, 5F12.4)') "bulk moduli (first 5 values): ", body%bulk_modulus(1:min(5,body%n_layers))
         end subroutine
 
 end module
